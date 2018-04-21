618d55602c3aef28281e3b2bb0951e1c8d3aa284
Elisabeth Narine <elisabethnarine@gmail.com>
2018-04-21 17:28:10 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,8 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
-    
+  elsif phrase == phrase.upcase
+  
 # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
