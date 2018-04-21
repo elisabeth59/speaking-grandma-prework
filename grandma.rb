98bea88583435f079709146c9694e7978be7cfa7
Elisabeth Narine <elisabethnarine@gmail.com>
2018-04-21 17:28:20 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -2,6 +2,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
+  "NO, NOT SINCE 1938!"
   
 # Write a speak_to_grandma method.
 
